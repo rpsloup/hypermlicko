7b06a59c6e2bd242f2afbdd516ac81dd0bbcbd5f
rpsloup <rpsloup@proton.me>
2023-03-10 19:06:18 +0100

    Creating a main SFML window

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main() {
-  std::cout << "Hello, world!" << std::endl;
+int main()
+{
+  sf::RenderWindow mainWindow(sf::VideoMode(400, 400), "SFML");
+  sf::CircleShape shape(100.f);
+  shape.setFillColor(sf::Color::Red);
+
+  while (mainWindow.isOpen())
+  {
+    sf::Event event;
+    while (mainWindow.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        mainWindow.close();
+    }
+
+    mainWindow.clear();
+    mainWindow.draw(shape);
+    mainWindow.display();
+  }
+
   return 0;
 }
