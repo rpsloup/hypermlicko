a7de6edc890b1170e8faf304e8988bc209326dbf
rpsloup <rpsloup@proton.me>
2023-03-10 19:04:28 +0100

    Adding SFML to the compile options

--- bake.sh
+++ bake.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+PROJECT_NAME='hypermlicko'
 COMPILER='g++'
 SOURCE_FOLDER='src'
 BUILD_FOLDER='build'
-PROJECT_NAME='hypermlicko'
+LIBRARIES='-lsfml-graphics -lsfml-window -lsfml-system'
 
 $COMPILER -c $SOURCE_FOLDER/Main.cpp
 mkdir -p $BUILD_FOLDER
-$COMPILER *.o -o $BUILD_FOLDER/$PROJECT_NAME
+$COMPILER *.o -o $BUILD_FOLDER/$PROJECT_NAME $LIBRARIES
 rm *.o
