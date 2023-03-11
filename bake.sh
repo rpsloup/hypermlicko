1ae314ed7e597befd5581bdec20b3b7e3543e047
rpsloup <rpsloup@proton.me>
2023-03-11 16:12:39 +0100

    Adding a run option to the compilation script

--- bake.sh
+++ bake.sh
@@ -8,6 +8,12 @@ CLASS_FOLDER="Classes"
 HEADER_FOLDER="Headers"
 LIBRARIES="-lsfml-graphics -lsfml-window -lsfml-system -lconfig++"
 
+if [ $1 = "run" ]
+then
+  ./$BUILD_FOLDER/$PROJECT_NAME
+  exit 0
+fi
+
 $COMPILER -c $SOURCE_FOLDER/Main.cpp $SOURCE_FOLDER/$CLASS_FOLDER/*.cpp
 mkdir -p $BUILD_FOLDER
 $COMPILER *.o -o $BUILD_FOLDER/$PROJECT_NAME -I $SOURCE_FOLDER/$HEADER_FOLDER $LIBRARIES
