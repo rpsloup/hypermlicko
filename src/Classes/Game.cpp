ef2d0760e1c1d3926dba6e1ad99a1864eeb1408c
rpsloup <rpsloup@proton.me>
2023-03-11 14:55:56 +0100

    Rendering a SFML window with the Game class

--- src/Classes/Game.cpp
+++ src/Classes/Game.cpp
@@ -1,15 +1,66 @@
 #include "../Headers/Game.h"
 
+// Constants
+const unsigned int WINDOW_WIDTH  = 800;
+const unsigned int WINDOW_HEIGHT = 600;
+const char*        WINDOW_TITLE  = "SFML";
+const unsigned int WINDOW_FPS    = 60;
+const bool         WINDOW_VSYNC  = false;
+
+// Initializers
+
+void Game::initWindow()
+{
+  this->window = new sf::RenderWindow(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
+  this->window->setVerticalSyncEnabled(WINDOW_VSYNC);
+  this->window->setFramerateLimit(WINDOW_FPS);
+}
+
 // Constructor
 
 Game::Game()
 {
-  std::cout << "Game." << std::endl;
+  this->initWindow();
+}
+
+// Update Functions
+
+void Game::updateSFMLEvent()
+{
+  while (this->window->pollEvent(this->sfEvent))
+  {
+    switch (this->sfEvent.type)
+    {
+      case sf::Event::Closed:
+        this->window->close();
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
+void Game::update()
+{
+  this->updateSFMLEvent();
+}
+
+// Render Functions
+
+void Game::render()
+{
+  this->window->clear();
+  this->window->display();
 }
 
 // Functions
 
 void Game::run()
 {
-  std::cout << "The game has been started." << std::endl;
+  while (this->window->isOpen())
+  {
+    this->update();
+    this->render();
+  }
 }
--- src/Headers/Game.h
+++ src/Headers/Game.h
@@ -2,11 +2,26 @@
 #define GAME_H
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 class Game
 {
+  private:
+    sf::RenderWindow* window;
+    sf::Event sfEvent;
+
+    // Initializers
+    void initWindow();
+
   public:
     Game();
+
+    // Update Functions
+    void updateSFMLEvent();
+    void update();
+
+    // Render Functions
+    void render();
 
     // Functions
     void run();
